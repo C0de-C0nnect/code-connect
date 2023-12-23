732c96708c298508c660e3e830c0ca2c2ae349b1
archieurzal <arcurza@gmail.com>
2023-12-23 15:36:50 +0800

    adding index and images

--- codeconnect/index.php
+++ codeconnect/index.php
@@ -1,10 +1,98 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
+   
+</head>
+<body class=".color-change-3x">
 
-include("config.php");
+<header>
+    <div class="user-profile">
+        <span class="profile-icon">
+            <img src="img/icons8-administrator-male-50.png" alt="Profile Image">
+        </span>
+      
+        Hi
+        <span class="username"> User Name</span>
 
-if(!isset($_SESSION['user'])){
-    header("location: login.php");
-}
-else{
-    header("location: dashboard.php");
-}
+    </div>
+    <div class="system-name">
+        
+        Code <Span class="con">Connect</Span>
+    </div>
+    <div class="search-bar">
+        <input type="text" placeholder="Search...">
+        <img class="search-icon" src="img/icons8-search-50.png" alt="Search Icon">    </div>
+</header>
+
+<div class="all-programmer">All <span class="prog"> Programmers</span></div>
+
+<div class="card-container">
+    <!-- Example of a card -->
+    <div class="card">
+        <div class="profile-icon">
+            <img src="img/icons8-administrator-male-50.png" alt="Profile Image">
+        </div>
+        <div class="card-name">
+            Programmers Name
+        </div>
+        <hr>
+<a href="programmer-details.html">
+        <div class="card-details">
+            Details 
+        </div>
+    </a>
+    </div>
+
+  
+        <div class="card">
+            <div class="profile-icon">
+                <img src="img/icons8-administrator-male-50.png" alt="Profile Image">
+            </div>
+            <div class="card-name">
+                Programmers Name
+            </div>
+            
+            <hr>
+    
+            <div class="card-details">
+                Details 
+            </div>
+        </div>   
+        <div class="card">
+            <div class="profile-icon">
+                <img src="img/icons8-administrator-male-50.png" alt="Profile Image">
+            </div>
+            <div class="card-name">
+                Programmers Name
+            </div>
+            <hr>
+    
+            <div class="card-details">
+                Details 
+            </div>
+        </div>    
+        <div class="card">
+            <div class="profile-icon">
+                <img src="img/icons8-administrator-male-50.png" alt="Profile Image">
+            </div>
+            <div class="card-name">
+                Programmers Name
+            </div>
+            <hr>
+    
+            <div class="card-details">
+                Details 
+            </div>
+        </div>
+    
+
+</div>
+
+
+
+
+</body>
+</html>
